fa7f428e46d059fa635deafd0178531ccf6dbaa5
dmachnia <01178558@pw.edu.pl>
2024-12-22 17:10:32 +0100

    Enhance FunnelError class with custom initialization and HTTP response conversion methods

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,10 +1,36 @@
 import funnel.exceptions as ex
 
 
-def test_error_to_json():
+def test_default_error():
     error = ex.FunnelError()
+    assert error.status_code == 500
+    assert error.error_reason == "Internal Server Error"
+    assert error.error_message == "An internal server error occurred."
+    assert error.additional_data == {}
 
-    assert error.to_json() == {
-        "error": "An internal server error occurred.",
+
+def test_custom_error():
+    error = ex.FunnelError("Custom error", {"key": "value"})
+    assert error.error_message == "Custom error"
+    assert error.additional_data == {"key": "value"}
+
+
+def test_error_to_dict():
+    error = ex.FunnelError("Custom error", {"key": "value"})
+    assert error.to_dict() == {
+        "error": "Custom error",
         "status_code": 500,
+        "key": "value",
+    }
+
+
+def test_error_to_http_response():
+    error = ex.FunnelError("Custom error", {"key": "value"})
+    response = error.to_http_response()
+    assert response.status_code == 500
+    assert response.reason == "Internal Server Error"
+    assert response.body == {
+        "error": "Custom error",
+        "status_code": 500,
+        "key": "value",
     }
