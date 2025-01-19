27a5dab93ce5a717946678dff60b6bfb8610033a
Natalia <npieczko23@gmail.com>
2025-01-19 23:36:12 +0100

    Add logging configuration and enhance ClientAddressFilter with docstrings

--- src/funnel/logger.py
+++ src/funnel/logger.py
@@ -1,14 +1,34 @@
+"""Logging configuration for the HTTP server"""
+
 import logging
 from logging.handlers import RotatingFileHandler
 
 
 class ClientAddressFilter(logging.Filter):
+    """Filter to add client address to log messages
+
+    Args:
+        client_ip (str): Client IP address
+        client_port (int): Client port number
+
+    Attributes:
+        client_ip (str): Client IP address
+        client_port (int): Client port number
+    """
     def __init__(self, client_ip=None, client_port=None):
         super().__init__()
         self.client_ip = client_ip
         self.client_port = client_port
 
-    def filter(self, record):
+    def filter(self, record: logging.LogRecord) -> bool:
+        """Filter to add client address to log messages
+
+        Args:
+            record (logging.LogRecord): Log record
+
+        Returns:
+            bool: True if the record is to be processed
+        """
         if self.client_ip and self.client_port:
             record.client_info = f"Client {self.client_ip}:{self.client_port}"
         else:
